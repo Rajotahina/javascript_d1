57223ae03053366179819a88206ea803edc8c5c0
rajotahina <rajotahina22@gmail.com>
2022-08-17 09:11:53 +0200

    version1

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-var color = "red";
-let name = "rasoa";
-const vitesse="10Km/h";
-color= "bleu";
-name= "rabe";
+// var color = "red";
+// let name = "rasoa";
+// const vitesse="10Km/h";
+// color= "bleu";
+// name= "rabe";
 /*function preparing() {
  //console.log("eating");
  return("eating");
@@ -29,19 +29,41 @@ console.log(color ,name,vitesse);
 // if ("blue "== "red") {
 //  console.log()
 // }
-function calculatrice (signe, a, b) {
-    if(signe == "addition") {
-     return a +b ;
-    }
-    if(signe == "soustration") {
-        return a -b ;
-    }
-     if(signe == "multiplication") {
-        return a *b ;
-        } 
+// function calculatrice (signe, a, b) {
+//     if(signe == "addition") {
+//      return a +b ;
+//     }
+//     if(signe == "soustration") {
+//         return a -b ;
+//     }
+//      if(signe == "multiplication") {
+//         return a *b ;
+//         } 
     
-    if (signe == "division") {
-        return a /b ;
-    } 
-    console.log(calculatrice());
+//     if (signe == "division") {
+//         return a /b ;
+//     } 
+//     console.log(calculatrice());
+// }
+/*let i = 1;
+let n = 5;
+do {
+    console.log(i);
+    i++;
+} while(i <= n);*/
+/*var legumes=["ovy,anana,carotte"];
+for(let pointer=0; pointer < legumes.length ;pointer++){
+    console.log(pointer);
+    console.log(legumes[pointer]);
+}*/
+var object =["clavier", "souris", "ecran"];
+for( let pointer = object.length-1; pointer >= 0; pointer--){
+    console.log(pointer);
+    console.log(object[pointer]);
 }
+object.push('cable');
+// console.log(object.join('_'));
+// console.log(object.pop());
+console.log(object);
+console.table(object);
+console.log(object.includes('omby'));
